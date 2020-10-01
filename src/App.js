3763cbf563e744c90aa0d90f50e323103e46e84e
Benjamin Miller <benmill@cs.utexas.edu>
2020-10-01 17:41:02 -0500

    dunno

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src="https://media.giphy.com/media/KCZ7edW5bF9akN2DgR/giphy.gif"  alt="logo" />
         <h2>
-          just looking for who asked
+          just looking for who asked...
         </h2>
         
       </header>
