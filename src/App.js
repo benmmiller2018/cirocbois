2607c1a101f6f54ba52a7bb6a0ffbcef0e860f48
Ben Miller <benmiller@Bens-MacBook-Air.local>
2022-09-24 17:52:13 -0500

    drive links

--- src/App.js
+++ src/App.js
@@ -1,142 +1,114 @@
 import React from 'react';
-import img1 from "./1.jpeg"
-import img2 from "./2.png"
-import img3 from "./3.PNG"
-import img4 from "./4.HEIC"
-import img5 from "./5.PNG"
-import img6 from "./6.JPG"
-import img7 from "./7.PNG"
-import img8 from "./8.JPG"
-import img9 from "./9.JPG"
-import img10 from "./10.PNG"
-import img11 from "./11.PNG"
-import img12 from "./12.PNG"
-import img13 from "./13.PNG"
-import img14 from "./14.HEIC"
-import img15 from "./15.PNG"
-import img16 from "./16.HEIC"
-import img17 from "./17.PNG"
-import img18 from "./18.JPG"
-import img19 from "./19.PNG"
-import img20 from "./20.JPG"
-import img21 from "./21.JPG"
-import img22 from "./22.JPG"
-import img23 from "./23.JPG"
-import img24 from "./24.JPG"
-import img25 from "./25.JPG"
-import img26 from "./26.PNG"
-import img27 from "./27.JPG"
-import img28 from "./joe.jpg"
-import img29 from "./mox.jpg"
-import img30 from "./pat.jpg"
-import img31 from "./patty.jpg"
+
 import './App.css';
 
 
 var d = new Date();
 var date = d.getDate();
-var img;/*
+var img;
 if (date == 1){
-  img = img1;
+  img="https://drive.google.com/uc?export=view&id=1voxPGokEgPMGjbbeSytS8Iy1PhU2C-_g";
 }
 else if (date == 2){
-  img = img2;
+  img="https://drive.google.com/uc?export=view&id=18RBCS3XARnux5hCzVepU1Cebuxrx-cdZ";
 }
 else if (date == 3){
-  img = img3;
+  img="https://drive.google.com/uc?export=view&id=1wyNsKLuYHEJfD6hQYlrHUvrKif7hTKkQ";
 }
 else if (date == 4){
-  img = img4;
+  img="https://drive.google.com/uc?export=view&id=1d-IYY4gTzkK_vfrCMxBGv8E3AdykMqUq";
 }
 else if (date == 5){
-  img = img5;
+  img="https://drive.google.com/uc?export=view&id=1BRovYBAhuiQf0swchrZWnEm4WPojocYi";
 }
 else if (date == 6){
-  img = img6;
+  img="https://drive.google.com/uc?export=view&id=1KV1iQ0rFtSDFvWRCL1DgqCjMYg1vP2fi";
 }
 else if (date == 7){
-  img = img7;
+  img="https://drive.google.com/uc?export=view&id=1Yfu07p1_HE4KXfmGnt9sxFCQx_hMCyuo";
 }
 else if (date == 8){
-  img = img8;
+  img="https://drive.google.com/uc?export=view&id=1RnfrIixeGvbTBZrTMwG9dR-w1kZIFqMg";
 }
 else if (date == 9){
-  img = img9;
+  img="https://drive.google.com/uc?export=view&id=15IcSIxvoLZI3LH3IK_7aswvsNniWQg6p";
 }
 else if (date == 10){
-  img = img10;
+  img="https://drive.google.com/uc?export=view&id=1xRYVot4tEuHKi_UAO_29AXy3-jJJFo5c";
 }
 else if (date == 11){
-  img = img11;
+  img="https://drive.google.com/uc?export=view&id=1itnwCqG9XZi1C4Jd65E9Z1XahQM92BUp";
 }
 else if (date == 12){
-  img = img12;
+  img="https://drive.google.com/uc?export=view&id=1GVbz0ShlngLm1u43XrgawBqMZNu0oehR";
 }
 else if (date == 13){
-  img = img13;
+  img="https://drive.google.com/uc?export=view&id=1W76YhEZg3YHZf2nOpr5FrPDaQmWX53Ee";
 }
 else if (date == 14){
-  img = img14;
+  img="https://drive.google.com/uc?export=view&id=1y2uVK2jqB4HdFAJ7mVBCIiDCrKNXzjYu";
 }
 else if (date == 15){
-  img = img15;
+  img="https://drive.google.com/uc?export=view&id=1q1IXWj3dNxWI2ZP9inS25ektgAlYkTaW";
 }
 else if (date == 16){
-  img = img16;
+  img="https://drive.google.com/uc?export=view&id=1sC1AA6y6V19WwlXiaCSpGjCoMa8WVkEe";
 }
 else if (date == 17){
-  img = img17;
+  img="https://drive.google.com/uc?export=view&id=1vJ78xY9U9btNOO5nA5kVMe1I0f5Ef3qX";
 }
 else if (date == 18){
-  img = img18;
+  img="https://drive.google.com/uc?export=view&id=12ZJnzWexGqELy6t7cLK6IB9_FJMN3fp1";
 }
 else if (date == 19){
-  img = img19;
+  img="https://drive.google.com/uc?export=view&id=1WYMV1xItq_Rvy3MuELAlsoF1IO6xv7bk";
 }
 else if (date == 20){
-  img = img20;
+  img="https://drive.google.com/uc?export=view&id=1WiUQgLgGbOOVRXfKrUQjvaad_kJe6FwV";
 }
 else if (date == 21){
-  img = img21;
+  img="https://drive.google.com/uc?export=view&id=1mQ9pEyZDQ53JEhNqypspw6rPioI3lTgY";
 }
 else if (date == 22){
-  img = img22;
+  //temp repeat
+  img="https://drive.google.com/uc?export=view&id=1EhpNg2oBG48thJFNvXy2X6fdwyrxsRut";
 }
 else if (date == 23){
-  img = img23;
+  //temp repeat
+  img="https://drive.google.com/uc?export=view&id=1EhpNg2oBG48thJFNvXy2X6fdwyrxsRut";
 }
 else if (date == 24){
-  img = img24;
+  img="https://drive.google.com/uc?export=view&id=18ACfuEOW95PYVGiS2_jdxziJ_Utk7w6h";
 }
 else if (date == 25){
-  img = img25;
+  img="https://drive.google.com/uc?export=view&id=1b1S5xw0SM9c0G7ONSv2Ip8x_liC_tYGp";
 }
 else if (date == 27){
-  img = img27;
+  img="https://drive.google.com/uc?export=view&id=1tPAeGgM3j1lPlFWG8yngmilkgN32AIT2";
 }
 else if (date == 28){
-  img = img28;
+  img="https://drive.google.com/uc?export=view&id=1fMMr6C2QVagBXHhM88dIfPvuNTZ32Euh";
 }
 else if (date == 29){
-  img = img29;
+  img="https://drive.google.com/uc?export=view&id=1JaoLGD1E5YW3wRFnqjcVvYpCKs3c79qN";
 }
 else if (date == 26){
-  img = img26;
+  img="https://drive.google.com/uc?export=view&id=16oYxeDBduwq9gWUPt8Y_XefF1Sqy0Yjm";
 }
 else if (date == 30){
-  img = img30;
+  img="https://drive.google.com/uc?export=view&id=1TSSekfl5iP7tFCsysV2XJUAf1_qL7rH1";
 }
 else{
-  img = img31;
+  img="https://drive.google.com/uc?export=view&id=1EhpNg2oBG48thJFNvXy2X6fdwyrxsRut";
 }
-*/
-img=img24;
+
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>ciroc boy of the day</h1>
         <img src={img} />
         
       </header>
